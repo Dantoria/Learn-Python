5cda0c76092a9e92dcf31e02fc9bba8047da918b
Dantoria <zaidan.meiluano@gmail.com>
2026-07-18 13:54:51 +0700

    Int and float data type

---
+++ data1.py
@@ -0,0 +1,15 @@
+#int and float
+
+# print(2 + 4)
+# print(2 - 4)
+# print(2 * 4)
+# print(2 / 4)
+
+# print(2 ** 4)
+# print(5 // 4)
+# print(6 % 4)
+
+#math functions
+
+print(round(3.1))
+print(abs(-20))

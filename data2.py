bdd70fff831e7268db7e964891571b763637027d
Dantoria <zaidan.meiluano@gmail.com>
2026-07-18 15:06:51 +0700

    long string and escape sequence

---
+++ data2.py
@@ -0,0 +1,16 @@
+username = "supercoder"
+password = "supersecret"
+long_string = '''
+WOW
+0 0
+---
+'''
+print(long_string)
+
+#escape sequence
+#\t = add tab spacing
+#\n = add new line
+
+weather = "\t the weather is sunny today \nenjoy your day"
+
+print(weather)

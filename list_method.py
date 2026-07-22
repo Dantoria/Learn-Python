855a0ee8e6cf4032fa5fe79f6a802c2ca1de6d47
Dantoria <zaidan.meiluano@gmail.com>
2026-07-22 19:56:28 +0700

    learning first list method

---
+++ list_method.py
@@ -0,0 +1,14 @@
+#list method
+
+basket = [1, 2, 3, 4, 5]
+
+basket.extend([100])
+
+basket.remove(3)
+print(basket)
+
+# append = add at end
+# insert = add at position
+# remove = remove by value
+# pop = remove by index
+# clear = remove all

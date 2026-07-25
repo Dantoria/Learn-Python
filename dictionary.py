ea0ce380bbf38c8bb4ef821a1ba56fb233303dfe
Dantoria <zaidan.meiluano@gmail.com>
2026-07-25 11:48:06 +0700

    learn dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+# dictionary
+
+my_list = [
+    {
+    'a' : [1, 2, 3],
+    'b' : 'hello',
+    'c' : True
+},
+{
+    'a' : [4, 5, 6],
+    'b' : 'hi',
+    'x' : False
+}
+]
+
+print(my_list[1]['a'][1])

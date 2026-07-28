66a861df2d41335f55e51f048afe35a52ef54018
Dantoria <zaidan.meiluano@gmail.com>
2026-07-28 19:38:55 +0700

    learn tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+# tuple = like a list, but, is immutable
+
+my_tuple = (1, 2, 3, 2, 2, 3, 1, 3, 2)
+print( 5 in my_tuple)
+
+# tuple can only use 2 methods; .count and .index
+
+print(my_tuple.count(2))
+print(my_tuple.index(3))

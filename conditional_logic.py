097b554dda8ae3aec027412fb44fdc78ae833e36
Dantoria <zaidan.meiluano@gmail.com>
2026-08-01 18:09:59 +0700

    learning conditional logic

---
+++ conditional_logic.py
@@ -0,0 +1,10 @@
+is_old = True
+is_licensed = False
+
+if is_old and is_licensed:
+    print("you're old enough to drive and got a license!!")
+
+else:
+    print("you're not old enough!!")
+
+print("yessir")

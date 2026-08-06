6172475b8f0fe81c2fb5f6fc19feb74171952748
Dantoria <zaidan.meiluano@gmail.com>
2026-08-06 20:24:14 +0700

    making project about conditional

---
+++ exercise_conditional.py
@@ -0,0 +1,11 @@
+is_magician = False
+is_expert = False
+
+if is_magician and is_expert:
+    print("you're a master magician")
+
+elif is_magician and not is_expert:
+    print("you're getting there")
+
+else:
+    print ("youre not a magician")
